61eda6570924297608bca339f627d91cbf48fc08
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2024-05-11 15:39:02 +0530

    logo change and javascript updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const main=document.getElementById('main_id');
+const main=document.getElementById('main_id1');
 let data;  
 async function getData(){
     const res=await fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8ea85bfbe82349b9a73206ce27fde029")
@@ -13,7 +13,7 @@ function News(){
     for(let i=0;i<20;i++){
         const article=document.createElement('article');
         article.innerHTML=`
-        <div class="arsistimage"><img src="${data[i].urlToImage}" alt="Singer Name" size="80%"></div>
+        <div class="arsistimage"><img src="${data[i].urlToImage}" alt="News Image" width="400px" height="140px"></div>
         <div class="name">${data[i].title}</div>
         <div class="about"></div>
         `
