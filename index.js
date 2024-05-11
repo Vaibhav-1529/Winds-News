7490c85215a424c7ef10a94cda66759e98d91f59
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2024-05-11 14:15:45 +0530

    java script updated

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ async function getData(){
     const res=await fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8ea85bfbe82349b9a73206ce27fde029")
     const result = await res.json();
     data = result.articles;
-    console.log("\ndata",data);
+    console.log("\ndata",result);
     News();
 }
 getData();
 function News(){
 
-    for(let i=0;i<10;i++){
+    for(let i=0;i<20;i++){
         const article=document.createElement('article');
         article.innerHTML=`
         <div class="arsistimage"><img src="${data[i].urlToImage}" alt="Singer Name" size="80%"></div>
-        <div class="name">Singer Name</div>
-        <div class="about">hello...</div>
+        <div class="name">${data[i].title}</div>
+        <div class="about"></div>
         `
         ;
         main.appendChild(article);
