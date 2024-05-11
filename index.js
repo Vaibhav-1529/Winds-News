be03d07857ab1d3e9485159a752fcba4f8524859
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2024-05-11 10:46:20 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const main=document.getElementById('main_id');
-let data;
+let data;  
 async function getData(){
     const res=await fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8ea85bfbe82349b9a73206ce27fde029")
-    const result=await res.json();
-    data=result.article;
-    console.log("\n :data:",data)
-
+    const result = await res.json();
+    data = result.articles;
+    console.log("\ndata",data);
     News();
 }
+getData();
 function News(){
 
     for(let i=0;i<10;i++){
         const article=document.createElement('article');
         article.innerHTML=`
-        <div class="arsistimage"><img src="${data[i].link}" alt="Singer Name"></div>
+        <div class="arsistimage"><img src="${data[i].urlToImage}" alt="Singer Name" size="80%"></div>
         <div class="name">Singer Name</div>
         <div class="about">hello...</div>
         `
@@ -21,4 +21,3 @@ function News(){
         main.appendChild(article);
     }
 }
-// News();
