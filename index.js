a9ff52ab0f59dfac00d02d4e05a4554e8e933e9a
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2024-05-11 16:22:20 +0530

    two row completed

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-const main=document.getElementById('main_id1');
-let data;  
-async function getData(){
-    const res=await fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8ea85bfbe82349b9a73206ce27fde029")
-    const result = await res.json();
-    data = result.articles;
-    console.log("\ndata",result);
-    News();
+const main1=document.getElementById('main_id1');
+const main2=document.getElementById('main_id2');
+let data1;  
+let data2;  
+
+async function getData1(){
+    const res1=await fetch("https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8ea85bfbe82349b9a73206ce27fde029")
+    const result1 = await res1.json();
+    data1 = result1.articles;
+    console.log("first API data\n");
+    console.log("\ndata",result1);
+    News1();
 }
-getData();
-function News(){
+getData1();
+function News1(){
 
     for(let i=0;i<20;i++){
         const article=document.createElement('article');
         article.innerHTML=`
-        <div class="arsistimage"><img src="${data[i].urlToImage}" alt="News Image" width="400px" height="140px"></div>
-        <div class="name">${data[i].title}</div>
+        <div class="arsistimage"><img src="${data1[i].urlToImage}" alt="News Image" width="400px" height="140px"></div>
+        <div class="name">${data1[i].title}</div>
         <div class="about"></div>
-        `
-        ;
-        main.appendChild(article);
+        ` ;
+        main1.appendChild(article);
+    }
+}
+async function getData2(){
+    const res2=await fetch("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=8ea85bfbe82349b9a73206ce27fde029")
+    const result2=await res2.json();
+    data2=result2.articles;
+    console.log("Second API data\n");
+    console.log("\ndata",result2);
+    News2();
+}
+getData2();
+function News2(){
+    for(let j=0;j<10;j++){
+        const article2=document.createElement('article');
+        article2.innerHTML=`
+        <div class="arsistimage"><img src="${data2[j].urlToImage}" alt="News Image" width="400px" height="140px"></div>
+        <div class="name">${data2[j].title}</div>
+        <div class="about"></div> `;
+        main2.appendChild(article2);
     }
 }
